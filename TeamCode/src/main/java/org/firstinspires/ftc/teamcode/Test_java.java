7b2f69a1a39a7b494668392886431813295a9e3f
Rafael <rafael.senk-jara@robolyon.com>
2025-11-08 21:59:36 +0100

    Test1 création

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_java.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+@TeleOp()
+
+public class Test_java extends LinearOpMode {
+    @Override
+    public void init() {
+        Telemetry.adddata
+    }
+
+
+
+}

85d0df8e87da0c121576d16eb4a0fb497f8e0c71
Rafael07SJ <rafael.senk-jara@robolyon.com>
2025-11-15 14:25:42 +0100

    Revert "construction du driver"
    This reverts commit 3f17269c0562983410150020b77ae714a030a8b0.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/euler/Constant.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.euler;
-
-public class Constant {
-    public final static String LEFT_MOTOR = "left motor";
-    public final static String RIGHT_MOTOR = "right motor";
-}

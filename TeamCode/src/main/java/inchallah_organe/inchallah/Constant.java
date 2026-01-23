835e71a07903eb668e6a7bcf5283476e79b405e9
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-01-23 19:05:32 +0100

    Update Constant.java

--- TeamCode/src/main/java/inchallah_organe/inchallah/Constant.java
+++ TeamCode/src/main/java/inchallah_organe/inchallah/Constant.java
@@ -1,11 +1,11 @@
 package inchallah_organe.inchallah;
 
 public class Constant {
-    public final static String LEFT_MOTOR = "jambe_gauche";
-    public final static String RIGHT_MORTOR = "jambe_droite";
-    public final static String INTAKE = "machoire";
-    public final static String SHOOTER = "fion";
-    public final static String FEEDER = "rectum";
+    public final static String LEFT_MOTOR = "left motor";
+    public final static String RIGHT_MORTOR = "right motor";
+    public final static String INTAKE = "intake";
+    public final static String SHOOTER = "shooter";
+    public final static String FEEDER = "feeder";
     public final static String VISEUR = "viseur";
     public final static String  COMPTEUR_BALLE = "compteur balles";
 }

71684d3b1e12c64e15715df84a9e5bb2664a19af
AxelGiroudon <axel.giroudon@robolyon.com>
2026-01-29 22:56:32 +0100

    pipi

--- TeamCode/src/main/java/Webcam_aldnc_yeux/Apriltag_reader.java
+++ TeamCode/src/main/java/Webcam_aldnc_yeux/Apriltag_reader.java
@@ -1,12 +1,16 @@
 package Webcam_aldnc_yeux;
 
 import android.graphics.Canvas;
+import android.util.Size;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.opencv.core.Mat;
@@ -14,11 +18,15 @@ import org.opencv.core.Mat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Apriltag_reader extends AprilTagProcessor {
+public class Apriltag_reader {
 
     public static AprilTagProcessor aprilTag ;
+    private  VisionPortal visionPortal;
+    private List<AprilTagDetection> detections = new ArrayList<>();
+    private Telemetry telemetry;
 
-    public Apriltag_reader() {
+    public Apriltag_reader(HardwareMap hardware, Telemetry telemetry) {
+        this.telemetry = telemetry;
 
          aprilTag = new AprilTagProcessor.Builder()
                 .setDrawTagID(true)
@@ -29,15 +37,24 @@ public class Apriltag_reader extends AprilTagProcessor {
                  .setLensIntrinsics(1666.94, 1666.94, 930.463, 618.081)
                 .build();
 
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardware.get(WebcamName.class, "Webcam 1"));
+        builder.addProcessor(aprilTag);
+        builder.setCameraResolution(new Size(640, 480));
+        //.setStreamFormat(VisionPortal.StreamFormat.YUY2)
+        //.setAutoStopLiveView(false)
+
+        visionPortal = builder.build();
+
 
 
     }
 
-    public static List<AprilTagDetection> getdetections() {
-        return aprilTag.getDetections();
+    public List<AprilTagDetection> getdetections() {
+        return detections;
     }
+    public void updtade (){detections = aprilTag.getDetections();}
     public AprilTagDetection getClosestApril() {
-        List<AprilTagDetection> detections = aprilTag.getDetections();
         if (detections.isEmpty()) {
             return null;
         }
@@ -54,8 +71,21 @@ public class Apriltag_reader extends AprilTagProcessor {
         return tag;
     }
 
+    public void telemetry(AprilTagDetection Detection){
+        if (Detection == null){return;}
+        if (Detection.metadata != null) {
+            telemetry.addLine(String.format("\n==== (ID %d) %s", Detection.id, Detection.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", Detection.ftcPose.x, Detection.ftcPose.y, Detection.ftcPose.z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", Detection.ftcPose.pitch, Detection.ftcPose.roll, Detection.ftcPose.yaw));
+            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", Detection.ftcPose.range, Detection.ftcPose.bearing, Detection.ftcPose.elevation));
+        } else {
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", Detection.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", Detection.center.x, Detection.center.y));
+        }
+    }
+
     public AprilTagDetection getAprilTagById(int wantedId) {
-        for (AprilTagDetection detection : aprilTag.getDetections()) {
+        for (AprilTagDetection detection : detections) {
             if (detection.id == wantedId) {
                 return detection;
             }
@@ -63,48 +93,13 @@ public class Apriltag_reader extends AprilTagProcessor {
         return null;
     }
 
-    public AprilTagDetection getBestAprilTag(Integer priorityId) {
-        return  priorityId != null ? getAprilTagById(priorityId) : getClosestApril();
+    public AprilTagDetection getBestAprilTag(int priorityId) {
+        return priorityId > 0 ? getAprilTagById(priorityId) : getClosestApril();
     }
 
-
-    @Override
-    public void setDecimation(float decimation) {
-
-    }
-
-    @Override
-    public void setPoseSolver(PoseSolver poseSolver) {
-
-    }
-
-    @Override
-    public int getPerTagAvgPoseSolveTime() {
-        return 0;
-    }
-
-    @Override
-    public ArrayList<AprilTagDetection> getDetections() {
-        return null;
-    }
-
-    @Override
-    public ArrayList<AprilTagDetection> getFreshDetections() {
-        return null;
-    }
-
-    @Override
-    public void init(int width, int height, CameraCalibration calibration) {
-
-    }
-
-    @Override
-    public Object processFrame(Mat frame, long captureTimeNanos) {
-        return null;
-    }
-
-    @Override
-    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
-
+    public void stop (){
+        if (visionPortal != null){
+            visionPortal.close();
+        }
     }
 }

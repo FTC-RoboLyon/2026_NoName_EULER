f1a691ff40533e72bb76c70197b66b2ee3afaf3d
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-02-15 15:41:20 +0100

    caca

--- TeamCode/src/main/java/Webcam_aldnc_yeux/Apriltag_reader.java
+++ TeamCode/src/main/java/Webcam_aldnc_yeux/Apriltag_reader.java
@@ -62,7 +62,7 @@ public class Apriltag_reader {
         AprilTagDetection tag = getAprilTagById(tagId);
         if (tag == null) return 0; // Aucun tag trouvé
         int imageWidth = 1920;
-        return (tag.center.x - (imageWidth / 2.0)) / (imageWidth / 2.0);
+        return (tag.center.x /*- (imageWidth / 2.0)) / (imageWidth / 2.0*/);
     }
 
     public List<AprilTagDetection> getdetections() {

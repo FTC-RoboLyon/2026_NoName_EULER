baa25ce92e8b602a80e117d37bfa894b583bc850
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-03-14 11:30:36 +0100

    Summary

--- TeamCode/src/main/java/FRC_ALDNC/Auto/Programme/AutoLyon.java
+++ TeamCode/src/main/java/FRC_ALDNC/Auto/Programme/AutoLyon.java
@@ -1,4 +1,20 @@
 package FRC_ALDNC.Auto.Programme;
 
-public class AutoLyon {
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import FRC_ALDNC.Auto.Container;
+
+
+@TeleOp(name = "AutoLyon", group = "FRC_ALDNC")
+public class Programme_Auto extends CommandOpMode {
+
+    Container container;
+
+    @Override
+    public void initialize() {
+
+        container = new Container(hardwareMap, telemetry, gamepad1);
+
+    }
 }

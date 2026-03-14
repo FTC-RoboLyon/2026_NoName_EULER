580a8553cda1a1c4a3761a67706e2e99e19fa403
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-03-14 11:34:52 +0100

    Update Container.java

--- TeamCode/src/main/java/FRC_ALDNC/Auto/Container.java
+++ TeamCode/src/main/java/FRC_ALDNC/Auto/Container.java
@@ -1,4 +1,30 @@
 package FRC_ALDNC.Auto;
 
-public class Container {
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import FRC_ALDNC.Auto.Command.DriveCommand;
+import FRC_ALDNC.Auto.Subsystems.DriveSubsystem;
+
+public class Container{
+    public double x, y;
+    Gamepad manette;
+    HardwareMap hmap;
+    Telemetry telemetry;
+    DriveSubsystem driveSubsystem;
+    public Container(HardwareMap hamp, Telemetry telemetry, Gamepad gamepad1){
+        this.telemetry = telemetry;
+        this.hmap = hamp;
+        manette = gamepad1;
+        driveSubsystem = new DriveSubsystem(hmap, telemetry);
+        driveSubsystem.setDefaultCommand(new DriveCommand(driveSubsystem, manette));
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
 }

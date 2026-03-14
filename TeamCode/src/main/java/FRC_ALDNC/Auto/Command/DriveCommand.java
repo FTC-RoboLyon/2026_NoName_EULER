04e5ef0063d6a04570311b9cd7cc4eaf98815227
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-03-14 11:28:01 +0100

    Update DriveCommand.java

--- TeamCode/src/main/java/FRC_ALDNC/Auto/Command/DriveCommand.java
+++ TeamCode/src/main/java/FRC_ALDNC/Auto/Command/DriveCommand.java
@@ -1,4 +1,20 @@
 package FRC_ALDNC.Auto.Command;
 
-public class DriveCommand {
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import FRC_ALDNC.Auto.Subsystems.DriveSubsystem;
+
+public class DriveCommand extends CommandBase {
+    Gamepad gamepad1;
+    DriveSubsystem driveSubsystem;
+    public DriveCommand(DriveSubsystem driveSubsystem, Gamepad gamepad1) {
+        this.driveSubsystem = driveSubsystem;
+        this.gamepad1 = gamepad1;
+        addRequirements(driveSubsystem);
+    }
+    @Override
+    public void execute(){
+        driveSubsystem.drive(gamepad1);
+    }
 }

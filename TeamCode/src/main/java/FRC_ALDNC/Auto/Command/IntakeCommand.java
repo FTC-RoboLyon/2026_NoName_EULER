b2ac9f048da2e03030a80648c202f6bd24a7943f
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-03-19 08:25:50 +0100

    Update IntakeCommand.java

--- TeamCode/src/main/java/FRC_ALDNC/Auto/Command/IntakeCommand.java
+++ TeamCode/src/main/java/FRC_ALDNC/Auto/Command/IntakeCommand.java
@@ -1,4 +1,31 @@
 package FRC_ALDNC.Auto.Command;
 
-public class IntakeCommand {
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import FRC_ALDNC.Auto.Subsystems.intakeSubsystem;
+
+public class intakeCommand extends CommandBase {
+    intakeSubsystem intakeSubsystem;
+    public enum intakage{OUI, NON, WAIT}
+    intakage mode;
+    public intakeCommand(intakeSubsystem intakeSubsystem, intakage mode){
+        this.mode = mode;
+        this.intakeSubsystem = intakeSubsystem;
+        addRequirements(intakeSubsystem);
+    }
+    @Override
+    public void execute() {
+        switch (mode) {
+            case OUI:
+                intakeSubsystem.configureIntake("Intake");
+                break;
+            case NON:
+                intakeSubsystem.configureIntake("Reject");
+                break;
+            case WAIT:
+                intakeSubsystem.configureIntake("Wait");
+                break;
+        }
+    }
 }

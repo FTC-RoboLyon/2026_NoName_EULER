2cfc4c75da4ce5a343e99b5125b8b80929ca46be
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-03-19 08:23:14 +0100

    Update AlignCommand.java

--- TeamCode/src/main/java/FRC_ALDNC/Auto/Command/AlignCommand.java
+++ TeamCode/src/main/java/FRC_ALDNC/Auto/Command/AlignCommand.java
@@ -1,4 +1,32 @@
 package FRC_ALDNC.Auto.Command;
 
-public class AlignCommand {
+import static FRC_ALDNC.CONSTAAANT_CESTMOILEBON.ff_rotation;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import ExercicesAntoineChatGPT.CameraSimple;
+import FRC_ALDNC.Auto.Subsystems.DriveSubsystem;
+import FRC_ALDNC.Auto.Subsystems.NavXSubsystem;
+import FRC_ALDNC.SubSystem.Camera_subsystem;
+
+public class AlignCommand extends CommandBase{
+    double ff_rotation = 0.11,p_rotation = 0.0025, erreurPos, turn, targetAngle;
+    DriveSubsystem driveSubsystem;
+    Camera_subsystem cameraSubsystem;
+    public AlignCommand(DriveSubsystem driveSubsystem, Camera_subsystem cameraSubsystem){
+        this.cameraSubsystem = cameraSubsystem;
+        this.driveSubsystem = driveSubsystem;
+        addRequirements(driveSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        targetAngle = cameraSubsystem.getBearing() == 100000 ? targetAngle : Math.toDegrees(driveSubsystem.getAngle())+cameraSubsystem.getBearing();
+        erreurPos = targetAngle - driveSubsystem.getAngle();
+        if (erreurPos > 180) erreurPos -= 2*Math.PI;
+        if (erreurPos < -180) erreurPos += 2*Math.PI;
+        turn = erreurPos*ff_rotation + erreurPos*p_rotation;
+        driveSubsystem.drive(0, turn);
+
+    }
 }

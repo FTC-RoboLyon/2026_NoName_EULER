b7543efee852e61bd343fc6015aca0d665cb6dd2
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-03-19 08:26:38 +0100

    Update ShooterCommand.java

--- TeamCode/src/main/java/FRC_ALDNC/Auto/Command/ShooterCommand.java
+++ TeamCode/src/main/java/FRC_ALDNC/Auto/Command/ShooterCommand.java
@@ -1,4 +1,32 @@
 package FRC_ALDNC.Auto.Command;
 
-public class ShooterCommand {
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.hardware.ams.AMSColorSensor;
+
+import FRC_ALDNC.Auto.Subsystems.ShooterSubsystem;
+
+public class ShooterCommand extends CommandBase {
+    ShooterSubsystem shooterSubsystem;
+    public enum shooterState{Bank, Mid, Far, Wait}
+    shooterState state;
+    public ShooterCommand(ShooterSubsystem shooterSubsystem, shooterState state){
+        this.shooterSubsystem = shooterSubsystem;
+        this.state = state;
+        addRequirements(shooterSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        shooterSubsystem.configureShooter(state);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return shooterSubsystem.getBallCount() >= 3;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooterSubsystem.configureShooter(shooterState.Wait);
+    }
 }

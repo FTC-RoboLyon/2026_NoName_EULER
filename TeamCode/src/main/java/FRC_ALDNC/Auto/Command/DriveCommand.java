b84f553d4e2719d4b11f0e6c439712a45235a63f
Antoinegrd11 <antoinegrd1108@gmail.com>
2026-03-19 08:24:19 +0100

    Update DriveCommand.java

--- TeamCode/src/main/java/FRC_ALDNC/Auto/Command/DriveCommand.java
+++ TeamCode/src/main/java/FRC_ALDNC/Auto/Command/DriveCommand.java
@@ -3,18 +3,51 @@ package FRC_ALDNC.Auto.Command;
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 import FRC_ALDNC.Auto.Subsystems.DriveSubsystem;
+import FRC_ALDNC.Auto.Subsystems.NavXSubsystem;
 
 public class DriveCommand extends CommandBase {
-    Gamepad gamepad1;
+    private double x, y;
+    Telemetry telemetry;
     DriveSubsystem driveSubsystem;
-    public DriveCommand(DriveSubsystem driveSubsystem, Gamepad gamepad1) {
+    public enum driveMode{ GoPos, GoAngle }
+    driveMode mode;
+    public DriveCommand(DriveSubsystem driveSubsystem, Telemetry telemetry, driveMode mode, double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.mode = mode;
+        this.telemetry = telemetry;
         this.driveSubsystem = driveSubsystem;
-        this.gamepad1 = gamepad1;
         addRequirements(driveSubsystem);
     }
     @Override
     public void execute(){
-        driveSubsystem.drive(gamepad1);
+        switch(mode){
+            case GoPos:
+                driveSubsystem.goPos(x,y);
+                break;
+            case GoAngle:
+                driveSubsystem.goAngle(x,y);
+                break;
+        }
+        telemetry.addData("targetX", x);
+        telemetry.addData("targetY", y);
+
+    }
+    @Override
+    public boolean isFinished(){
+        if(mode == driveMode.GoAngle){
+            return driveSubsystem.getAngleTo() < 0.02;
+        }else {
+            return driveSubsystem.getDistanceTo(x,y) < 1.5;
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        driveSubsystem.right_motor_power = 0;
+        driveSubsystem.left_motor_power = 0;
     }
 }

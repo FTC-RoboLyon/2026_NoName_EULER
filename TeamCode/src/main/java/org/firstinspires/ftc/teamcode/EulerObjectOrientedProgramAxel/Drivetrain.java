9992c6dd05b3d3f876e64c6931d12110bc1b7a72
AxelGiroudon <axel.giroudon@robolyon.com>
2026-08-29 13:09:31 +0200

    ouais ouais

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EulerObjectOrientedProgramAxel/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EulerObjectOrientedProgramAxel/Drivetrain.java
@@ -2,11 +2,9 @@ package org.firstinspires.ftc.teamcode.EulerObjectOrientedProgramAxel;
 
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.Lib.utils;
 
 public class Drivetrain {
@@ -38,10 +36,11 @@ public class Drivetrain {
     private double frontRightPower;
     private double backLeftPower;
     private double backRightPower;
-    private ElapsedTime PIDTimer = new ElapsedTime();
-    private double previousPDError = 0.0;
-    private double previousPDTime = 0.0;
+    private double forward = 0.0, strafe = 0.0, turn = 0.0;
+    private ElapsedTime goToPosTimer = new ElapsedTime();
+    private ElapsedTime headingTimer = new ElapsedTime();
     private double previousGoPosTime = 0.0;
+    private double previousHeadingTime = 0.0;
     double previousLeftValue = 0;
     double previousRightValue = 0;
     double previousStrafeValue = 0;
@@ -80,7 +79,8 @@ public class Drivetrain {
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        PIDTimer.startTime();
+        goToPosTimer.startTime();
+        headingTimer.startTime();
     }
     public Drivetrain (HardwareMap hmap, SparkFunOTOS.Pose2D startPos){
         this(hmap);
@@ -88,13 +88,27 @@ public class Drivetrain {
         robotY = startPos.y;
         robotHeading = startPos.h;
     }
-    public void Drive (double turn, double forward, double strafe, boolean fieldOriented){
 
-        if (fieldOriented){
-            double forwardCopy = forward; //;)
-            forward = Math.cos(robotHeading)*forwardCopy + Math.sin(robotHeading)*strafe;
-            strafe = -Math.sin(robotHeading)*forwardCopy + Math.cos(robotHeading)*strafe;
+    /**
+     * Move the drivetrain using
+     * @param Turn the power with which the robot will turn
+     * @param Forward the power with which the robot will move forward
+     * @param Strafe the power with which the robot will move sideway
+     *               For the three parameters above, if we give the value of 1000.0, the precedents values given will be kept
+     * @param fieldOriented if we want to transform Forward and Strafe power from field coordinate to robot coordinate
+     */
+    public void Drive (double Turn, double Forward, double Strafe, boolean fieldOriented){
+        if(Forward != 1000.0 && Strafe != 1000.0){
+            if (fieldOriented){
+                forward = Math.cos(robotHeading)*Forward + Math.sin(robotHeading)*Strafe;
+                strafe = -Math.sin(robotHeading)*Forward + Math.cos(robotHeading)*Strafe;
+            }else{
+                forward = Forward;
+                strafe = Strafe;
+            }
         }
+        if (Turn != 1000.0)
+            turn = Turn;
 
         double maxMotorValue = Math.max(Math.abs(turn) + Math.abs(forward) + Math.abs(strafe), 1);
 
@@ -107,71 +121,6 @@ public class Drivetrain {
         frontRightMotor.setPower(frontRightPower);
         backLeftMotor.setPower(backLeftPower);
         backRightMotor.setPower(backRightPower);
-    }
-
-
-    //Les docs ca ressemble plutot a ca (meme si la elle peuvent encore etre mieux) :
-    /**
-     * A function that allows the robot to move to a given point of coordinates (xTarget, yTarget) and head to a given heading target.
-     * Return if the robot has arrived yet using tolerances.
-     * @param xTarget X coordinate of the target point (in meters)
-     * @param yTarget Y coordinate of the target point (in meters)
-     * @param headingTarget heading target of the robot (in radians)
-     * @return if the robot has arrived yet using tolerances (true : yes; false : no)
-     */
-    public boolean goToPos (double xTarget, double yTarget, double headingTarget) {
-        //return true if the robot is already at the giving target point and heading
-        if (utils.IsInRange(robotX, xTarget, TOLERANCE_X_AND_Y)
-                && utils.IsInRange(robotY, yTarget, TOLERANCE_X_AND_Y)
-                && utils.IsInRange(robotHeading, headingTarget, TOLERANCE_HEADING))
-        {
-            return true;
-        }
-
-        double xError = xTarget - robotX;
-        double yError = yTarget - robotY;
-        double headingError = headingTarget - robotHeading;
-
-        double xErrorCopy = xError;
-        xError = Math.cos(robotHeading)*xErrorCopy + Math.sin(robotHeading)*yError;
-        yError = -Math.sin(robotHeading)*xErrorCopy + Math.cos(robotHeading)*yError;
-
-        double pTermX = KP_FORWARD * xError;
-        double pTermY = KP_STRAFE * yError;
-        double pTermHeading = KP_TURN * headingError;
-
-        double actualTime = PIDTimer.milliseconds();
-        double dTermX = KD_FORWARD * ((xError - previousXError)/(actualTime - previousGoPosTime));
-        double dTermY = KD_STRAFE * ((yError - previousYError)/(actualTime - previousGoPosTime));
-        double dTermHeading = KD_TURN * ((headingError - previousHeadingError)/(actualTime - previousGoPosTime));
-
-        double forward = pTermX + dTermX;
-        double strafe = pTermY + dTermY;
-        double turn = pTermHeading + dTermHeading;
-
-        Drive(turn, forward, strafe, false);
-
-        previousXError = xError;
-        previousYError = yError;
-        previousHeadingError = headingError;
-        previousGoPosTime = actualTime;
-
-        return false;
-    }
-
-    public void headToTarget(double error, double forward, double strafe){
-
-        double pTerm = KP_AUTO_ALIGN * error;
-
-        double actualTime = PIDTimer.milliseconds();
-        double dTerm = KD_AUTO_ALIGN * ((error - previousPDError)/(actualTime - previousPDTime));//et il se passe quoi s'il n'y avait pas de previousPDTime ou de previousPDError
-
-        double turn = pTerm + dTerm;
-
-        Drive(turn, forward, strafe, true);
-
-        previousPDError = error;
-        previousPDTime = actualTime;
     }
 
     public void actualiseRobotPos (){
@@ -199,6 +148,85 @@ public class Drivetrain {
         previousLeftValue = leftPodValue;
         previousRightValue = rightPodValue;
         previousStrafeValue = strafePodValue;
+    }
+
+
+    //Les docs ca ressemble plutot a ca (meme si la elle peuvent encore etre mieux) :
+    /**
+     * A function that allows the robot to move to a given point of coordinates (xTarget, yTarget) while turning itself freely.
+     * Return if the robot has arrived yet using tolerances.
+     * @param xTarget X coordinate of the target point (in meters)
+     * @param yTarget Y coordinate of the target point (in meters)
+     * @param turn the rotation given directly to the robot (if you don't want the robot to turn just set it to 0)
+     *                  For the parameter above, if we give the value of 1000.0, the precedent value given will be kept
+     * @return if the robot has arrived yet using tolerances (true : yes; false : no)
+     */
+    public boolean goToPos (double xTarget, double yTarget, double turn) {
+        //return true if the robot is already at the giving target point and heading
+        if (utils.IsInRange(robotX, xTarget, TOLERANCE_X_AND_Y)
+                && utils.IsInRange(robotY, yTarget, TOLERANCE_X_AND_Y)) {
+            return true;
+        }
+
+        double xError = xTarget - robotX;
+        double yError = yTarget - robotY;
+
+        double xErrorCopy = xError;
+        xError = Math.cos(robotHeading) * xErrorCopy + Math.sin(robotHeading) * yError;
+        yError = -Math.sin(robotHeading) * xErrorCopy + Math.cos(robotHeading) * yError;
+
+        double pTermX = KP_FORWARD * xError;
+        double pTermY = KP_STRAFE * yError;
+
+        double actualTime = goToPosTimer.milliseconds();
+        double dTermX = KD_FORWARD * ((xError - previousXError) / (actualTime - previousGoPosTime));
+        double dTermY = KD_STRAFE * ((yError - previousYError) / (actualTime - previousGoPosTime));
+
+        double forward = pTermX + dTermX;
+        double strafe = pTermY + dTermY;
+
+        Drive(turn, forward, strafe, false);
+
+        previousXError = xError;
+        previousYError = yError;
+        previousGoPosTime = actualTime;
+
+        return false;
+    }
+    /**
+     * A function that allows the robot to orient itself to a given orientation while moving along x and y axises
+     * Return if the robot is oriented yet using tolerance
+     * @param headingTarget the orientation that we want the robot to be
+     * @param forward the power with which the robot will move forward
+     * @param strafe the power with which the robot will move sideway
+     * @return if the robot is oriented yet using tolerance (true : yes ; false : no)
+     */
+    public boolean headToTarget(double headingTarget, double forward, double strafe){
+
+        if (utils.IsInRange(robotHeading, headingTarget, TOLERANCE_HEADING))
+            return true;
+
+        double headingError = headingTarget - robotHeading;
+
+        double pTermHeading = KP_TURN * headingError;
+
+        double actualTime = goToPosTimer.milliseconds();
+        double dTermHeading = KD_TURN * ((headingError - previousHeadingError)/(actualTime - previousHeadingTime));
+
+        double turn = pTermHeading + dTermHeading;
+
+        Drive(turn, forward, strafe, true);
+        previousHeadingError = headingError;
+        previousHeadingTime = actualTime;
+
+        return false;
+    }
+
+    public boolean goToPosAndHead (double xTarget, double yTarget, double headingTarget){
+        if (goToPos(xTarget, yTarget, 1000.0) && headToTarget(headingTarget, 1000.0, 1000.0)){
+            return true;
+        }
+        return false;
     }
 
     public double getRobotHeading(){
